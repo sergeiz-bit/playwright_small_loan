22954a5fb8a4f6de8089c4e65b7a43da44083373
user <sergei.zahharov1@gmail.com>
2025-03-13 17:34:41 +0200

    HW-21 | add negative tests

--- tests/mock.spec.ts
+++ tests/mock.spec.ts
@@ -6,13 +6,11 @@ test.describe("Loan app mock tests", async () => {
 
         const expectedMonthlyAmount = 100005
         const smallLoanPage = new SmallLoanPage(page);
-        await page.route("**/api/loan-calc*", async(request)=>{
+        await page.route("**/api/loan-calc*", async (request) => {
 
             const responseBody = {paymentAmountMonthly: expectedMonthlyAmount}
             await request.fulfill({
-                status: 200,
-                contentType: "application/json",
-                body: JSON.stringify(responseBody)
+                status: 200, contentType: "application/json", body: JSON.stringify(responseBody)
             })
         })
         const responsePromise = page.waitForResponse("**/api/loan-calc*");
@@ -20,6 +18,52 @@ test.describe("Loan app mock tests", async () => {
         await responsePromise;
         await smallLoanPage.checkMonthlyAmount(expectedMonthlyAmount)
 
+    });
+
+    test('TL-21-2 Response 500 with empty body', async ({page}) => {
+        const smallLoanPage = new SmallLoanPage(page);
+        await page.route("**/api/loan-calc*", async (request) => {
+
+            const responseBody = {}
+            await request.fulfill({
+                status: 500, contentType: "application/json", body: JSON.stringify(responseBody)
+            })
+        })
+        const responsePromise = page.waitForResponse("**/api/loan-calc*");
+        await smallLoanPage.open();
+        await responsePromise;
+        await smallLoanPage.checkErrorMessageDisplayed();
+    });
+
+    test('TL-21-3 Response 200 with empty body', async ({page}) => {
+        const smallLoanPage = new SmallLoanPage(page);
+        await page.route("**/api/loan-calc*", async (request) => {
+
+            const responseBody = {}
+            await request.fulfill({
+                status: 200, contentType: "application/json", body: JSON.stringify(responseBody)
+            })
+        })
+        const responsePromise = page.waitForResponse("**/api/loan-calc*");
+        await smallLoanPage.open();
+        await responsePromise;
+        await smallLoanPage.checkMonthlyAmountUndefined();
+    });
+
+    test('TL-21-4 Response 200 with wrong key', async ({page}) => {
+        const expectedMonthlyAmount = 100005
+        const smallLoanPage = new SmallLoanPage(page);
+        await page.route("**/api/loan-calc*", async (request) => {
+
+            const responseBody = {wrongKey: expectedMonthlyAmount}
+            await request.fulfill({
+                status: 200, contentType: "application/json", body: JSON.stringify(responseBody)
+            })
+        })
+        const responsePromise = page.waitForResponse("**/api/loan-calc*");
+        await smallLoanPage.open();
+        await responsePromise;
+        await smallLoanPage.checkMonthlyAmountUndefined();
     });
 
 })
